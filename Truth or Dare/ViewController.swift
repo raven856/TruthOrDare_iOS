43c6589e27e26c7ab1ed411a2bb0464654732d6b
Ralph Venuto <ralphvenuto@Ralphs-MacBook-Pro.local>
2017-02-09 22:38:51 -0500

    Initial Commit

---
+++ Truth or Dare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Truth or Dare
+//
+//  Created by Ralph Venuto on 2/9/17.
+//  Copyright © 2017 Ralph Venuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
